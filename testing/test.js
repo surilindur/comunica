4b586224a22ff1dd48752f39ebf742d3b4e68ae9
surilindur <16085353+surilindur@users.noreply.github.com>
2024-10-08 21:11:02 +0200

    Add small test script

---
+++ testing/test.js
@@ -0,0 +1,47 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { KeysHttp } from '@comunica/context-entries';
+import { LoggerPretty } from '@comunica/logger-pretty';
+import { QueryEngine } from '@comunica/query-sparql';
+
+const engine = new QueryEngine();
+
+const query = `#uniprot38: https://sparql.uniprot.org/.well-known/sparql-examples/
+
+PREFIX uniprotkb: <http://purl.uniprot.org/uniprot/>
+PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+PREFIX pq: <http://www.wikidata.org/prop/qualifier/>
+PREFIX p: <http://www.wikidata.org/prop/>
+PREFIX ps: <http://www.wikidata.org/prop/statement/>
+
+SELECT ?protein ?begin ?end ?chromosome ?assembly WHERE {
+  {
+    BIND(uniprotkb:P05067 AS ?proteinIRI)
+    BIND (SUBSTR(STR(?proteinIRI), STRLEN(STR(uniprotkb:))+1) AS ?protein)
+  }
+  #SERVICE <https://query.wikidata.org/sparql> {
+    ?wp wdt:P352 ?protein ;
+        wdt:P702 ?wg .
+    ?wg p:P644   ?wgss .
+    ?wgss ps:P644        ?begin ;
+      pq:P1057/wdt:P1813 ?chromosome ;
+      pq:P659/rdfs:label ?assembly .
+    ?wg p:P645 ?wgse .
+    ?wgse ps:P645        ?end ;
+      pq:P1057/wdt:P1813 ?chromosome ;
+      pq:P659/rdfs:label ?assembly .
+    FILTER(lang(?assembly) = "en")
+  #}
+}`;
+
+const bindingsStream = await engine.queryBindings(query, {
+  sources: [ 'https://query.wikidata.org/sparql', 'https://sparql.rhea-db.org/sparql' ],
+  httpRetryCount: 10,
+  httpRetryDelayFallback: 1_000,
+  [KeysHttp.httpRetryStatusCodes.name]: [ 500 ],
+  log: new LoggerPretty({ level: 'debug' }),
+});
+
+const bindings = await bindingsStream.toArray();
+
+// eslint-disable-next-line no-console
+console.log(bindings);
