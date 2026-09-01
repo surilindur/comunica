4f9c06bf448ce72debbd1b931826ea41814b426d
surilindur <16085353+surilindur@users.noreply.github.com>
2026-09-01 17:51:17 +0200

    Adjust numeric value handling in xsd:string to follow the specification

--- packages/actor-function-factory-term-xsd-to-string/lib/TermFunctionXsdToString.ts
+++ packages/actor-function-factory-term-xsd-to-string/lib/TermFunctionXsdToString.ts
@@ -6,7 +6,10 @@ import type {
 import {
   bool,
   declare,
+  decimal,
+  double,
   float,
+  integer,
   string,
   TypeURL,
 } from '@comunica/utils-expression-evaluator';
@@ -20,7 +23,30 @@ export class TermFunctionXsdToString extends TermFunctionBase {
       arity: 1,
       operator: TypeURL.XSD_STRING,
       overloads: declare(TypeURL.XSD_STRING)
-        .onNumeric1(() => (val: NumericLiteral) => string(float(val.typedValue).str()))
+        .onNumeric1(() => (val: NumericLiteral) => {
+          if (val.dataType === TypeURL.XSD_INTEGER) {
+            return string(integer(val.typedValue).str());
+          }
+          if (val.dataType === TypeURL.XSD_DECIMAL) {
+            return string(decimal(val.typedValue).str());
+          }
+          if (val.dataType === TypeURL.XSD_DOUBLE || val.dataType === TypeURL.XSD_FLOAT) {
+            // When the value is zero, it should be returned as "0"
+            if (val.typedValue === 0) {
+              return string('0');
+            }
+            const absoluteValue = Math.abs(val.typedValue);
+            if (absoluteValue >= 1e-6 && absoluteValue < 1e6) {
+              // Doubles and floats with absolute values in [1e-6, 1e6[
+              // must be cast to decimal before casting to string.
+              return string(decimal(val.typedValue).str());
+            }
+            if (val.dataType === TypeURL.XSD_DOUBLE) {
+              return string(double(val.typedValue).str());
+            }
+          }
+          return string(float(val.typedValue).str());
+        })
         .onBoolean1Typed(() => val => string(bool(val).str()))
         .onTerm1(() => (val: StringLiteral) => string(val.str()))
         .collect(),
